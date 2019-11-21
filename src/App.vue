f3ef21958a26cc84d58e3f521734edb2925a1ff6
Hendrikus van Katwijk <hendrikus@resttech.io>
2019-11-21 12:44:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos:[
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:3,
+          title:"Todo Two",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Three",
+          completed:false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing:border-box;
+  margin:0;
+  padding:0;
+}
+
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    names:"Todos"
+}
+</script>
+
+<style scoped>
+
+</style>
