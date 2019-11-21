e39f6f5b6c78c782c624d7c40ac858853e6b7a67
Hendrikus van Katwijk <hendrikus@resttech.io>
2019-11-21 13:58:37 +0100

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 import Header from './components/layout/Header'
 
 export default {
   name: 'app',
   components: {
     Todos,
-    Header
+    Header,
+    AddTodo
   },
   data(){
     return{
@@ -39,22 +42,36 @@ export default {
   methods:{
     deleteTodo(id){
       this.todos = this.todos.filter(todo=>todo.id!== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos,newTodo];
     }
   }
 }
 </script>
 
 <style>
-*{
-  box-sizing:border-box;
-  margin:0;
-  padding:0;
-}
+  *{
+    box-sizing:border-box;
+    margin:0;
+    padding:0;
+  }
 
-body {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  .btn{
+    display:inline-block;
+    border:none;
+    background: #555;
+    color:#fff;
+    padding:7px 20px;
+    cursor:pointer;
+  }
+  .btn:hover{
+    background:#666
+  }
 </style>
