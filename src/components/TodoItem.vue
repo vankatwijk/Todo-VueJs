a9c78e8c6502be3cbc69a9232b95dec2e0b8429f
Hendrikus van Katwijk <hendrikus@resttech.io>
2019-11-21 13:16:43 +0100

    markcomplete works

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,21 @@
 <template>
     <div class='todo-item' v-bind:class="{'is-complete':todo.completed}">
-        <p>{{todo.title}}</p>
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+        </p>
     </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props:["todo"]
+    props:["todo"],
+    methods:{
+        markComplete(){
+           this.todo.completed = !this.todo.completed
+        }
+    }
 }
 </script>
 
